f145301e12d1f70ae672cd574210fb1e5fe02019
Erik Bjørnsen <bjornsen.erik@gmail.com>
2024-10-18 14:21:06 +0200

    bug: fixed array format, tfplan in actions

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -11,5 +11,5 @@ resource "azurerm_subnet" "subnet" {
   name = format("%s%d", var.subnet_name_prefix, count.index)
   resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes = format("[10.0.%d.0/24]", count.index)
+  address_prefixes = [format("10.0.%d.0/24", count.index)]
 }
