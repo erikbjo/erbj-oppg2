cdd03f707a50e24075ba608b8e1d833200dfa241
Erik Bjørnsen <bjornsen.erik@gmail.com>
2024-11-04 14:29:17 +0100

    chore: added broader restrictions to provider versions :dog:

--- deployments/terraform.tf
+++ deployments/terraform.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "4.0.1"
+      version = "~>4.8.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "3.6.2"
+      version = "~>3.6.3"
     }
   }
 
